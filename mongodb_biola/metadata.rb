fdca3b5d5bc179c9d91fcd199b86aafcf839839b
Will B <theborch@gmail.com>
2012-08-08 10:31:13 -0700

    Enabled auth and nohttpinterface.

--- mongodb_biola/metadata.rb
+++ mongodb_biola/metadata.rb
@@ -1,5 +1,5 @@
 maintainer       "Biola"
 maintainer_email "will.borchardt@biola.edu"
 description      "Installs/Configures mongodb"
-version          "0.1.1"
+version          "0.1.2"
 depends          "apt"
