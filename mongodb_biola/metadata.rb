8bb09e9d4ad37ce6095c6daea74e3f5b40d6f292
Will B <theborch@gmail.com>
2012-08-08 10:10:43 -0700

    Added service restart upon template change to install recipes.

--- mongodb_biola/metadata.rb
+++ mongodb_biola/metadata.rb
@@ -1,5 +1,5 @@
 maintainer       "Biola"
 maintainer_email "will.borchardt@biola.edu"
 description      "Installs/Configures mongodb"
-version          "0.1.0"
+version          "0.1.1"
 depends          "apt"
