4622bec4f04941d1e5cca31312dd71e1c8ee3984
troyready <troy@troyready.com>
2013-01-07 16:18:29 -0800

    logrotate_misc: initial commit

---
+++ logrotate_misc/README.md
@@ -0,0 +1,13 @@
+Description
+===========
+Simple cookbook to hold recipes for logrotate configurations that don't fit in other recipes.
+
+Requirements
+============
+
+Attributes
+==========
+
+Usage
+=====
+
---
+++ logrotate_misc/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Biola University"
+maintainer_email "troy.ready@biola.edu"
+license          "Apache 2.0"
+description      "Miscellaneous recipies for logrotate configuration"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+depends          "logrotate"
