d32c6127882e33d1706f3e5be88aee7da180e8c7
Garry Cheng <garry.cheng@biola.edu>
2013-04-29 14:22:40 -0700

    Created default recipe

--- postfix_biola/recipes/default.rb
+++ postfix_biola/recipes/default.rb
@@ -6,3 +6,20 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+service "postfix" do
+	supports :status => true, :stop => true, :start => true, :reload => true, :restart => true, :status => true, :force-reload => true,
+end
+
+postfix_whitelist = Array.new
+
+postfix_whitelist = search(:node, "relayhost_role:postfix_relayhost")
+
+template "/etc/postfix/whitelist" do
+	source whitelist.erb
+	owner "root"
+	group "root"
+	mode "00644"
+	variables	:postfix_whitelist => postfix_whitelist
+	notifies :force-reload, "service[postfix]"
+end
