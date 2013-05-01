8b61f61f3e345d8e15057d8bb85184b4beaf7b6e
Garry Cheng <garry.cheng@biola.edu>
2013-04-30 17:18:12 -0700

    Added ability to have devices whitelisted when added to the postfix_whitelist databag.

--- postfix_biola/recipes/default.rb
+++ postfix_biola/recipes/default.rb
@@ -15,11 +15,16 @@ postfix_whitelist = Array.new
 
 postfix_whitelist = search(:node, "postfix_relayhost_role:postfix_relayhost")
 
+device_whitelist = Array.new
+
+device_whitelist = search(:postfix_whitelist, "*:*")
+
 template "/etc/postfix/whitelist" do
 	source "whitelist.erb"
 	owner "root"
 	group "root"
 	mode "00644"
-	variables	:postfix_whitelist => postfix_whitelist
+	variables 	:postfix_whitelist => postfix_whitelist,
+	:device_whitelist => device_whitelist
 	notifies :reload, "service[postfix]"
 end
