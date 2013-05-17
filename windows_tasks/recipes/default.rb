d3f4065f04161290109d9eaab8950b0424326630
troyready <troy@troyready.com>
2013-05-17 14:52:28 -0700

    windows_tasks: add proper prereq checks for attribute occurance

--- windows_tasks/recipes/default.rb
+++ windows_tasks/recipes/default.rb
@@ -17,19 +17,23 @@
 # limitations under the License.
 #
 
-node['windows']['scheduled_tasks'].each_with_index do |parameters, index|
-  parameters.each_key do |task_name|
-    windows_task parameters.keys[0].to_s do
-      user parameters[task_name]['user'] if parameters[task_name]['user']
-      password parameters[task_name]['password'] if parameters[task_name]['password']
-      cwd parameters[task_name]['cwd'] if parameters[task_name]['cwd']
-      command parameters[task_name]['command'] if parameters[task_name]['command']
-      action parameters[task_name]['action'].to_sym if parameters[task_name]['action']
-      run_level parameters[task_name]['run_level'].to_sym if parameters[task_name]['run_level']
-      frequency parameters[task_name]['frequency'].to_sym if parameters[task_name]['frequency']
-      frequency_modifier parameters[task_name]['frequency_modifier'] if parameters[task_name]['frequency_modifier']
-      start_day parameters[task_name]['start_day'] if parameters[task_name]['start_day']
-      start_time parameters[task_name]['start_time'] if parameters[task_name]['start_time']
+if node['windows']
+  if node['windows']['scheduled_tasks']
+    node['windows']['scheduled_tasks'].each_with_index do |parameters, index|
+      parameters.each_key do |task_name|
+        windows_task parameters.keys[0].to_s do
+          user parameters[task_name]['user'] if parameters[task_name]['user']
+          password parameters[task_name]['password'] if parameters[task_name]['password']
+          cwd parameters[task_name]['cwd'] if parameters[task_name]['cwd']
+          command parameters[task_name]['command'] if parameters[task_name]['command']
+          action parameters[task_name]['action'].to_sym if parameters[task_name]['action']
+          run_level parameters[task_name]['run_level'].to_sym if parameters[task_name]['run_level']
+          frequency parameters[task_name]['frequency'].to_sym if parameters[task_name]['frequency']
+          frequency_modifier parameters[task_name]['frequency_modifier'] if parameters[task_name]['frequency_modifier']
+          start_day parameters[task_name]['start_day'] if parameters[task_name]['start_day']
+          start_time parameters[task_name]['start_time'] if parameters[task_name]['start_time']
+        end
+      end
     end
   end
 end
