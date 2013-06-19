ceba8497cf3f113702916c9fa8654acece061c9f
troyready <troy@troyready.com>
2013-06-18 18:11:02 -0700

    oracle: v0.5.0 (corrected RHEL6 x64 support)

--- oracle/recipes/packages.rb
+++ oracle/recipes/packages.rb
@@ -29,32 +29,14 @@ when "rhel"
   # Special exceptions here for RHEL 5/6
   if node['platform_version'].to_i >= 6
     yum_package 'mksh'
-    if node['kernel']['machine'] == "i686"
-      yum_package 'openmotif'
-    else
-      ['x86_64','i686'].each do |architecture|
-        yum_package 'openmotif' do
-          arch architecture
-        end
-      end
-    end
   else
     yum_package 'pdksh'
-    if node['kernel']['machine'] == "i686"
-      yum_package 'openmotif'
-    else
-      ['x86_64','i386'].each do |architecture|
-        yum_package 'openmotif' do
-          arch architecture
-        end
-      end
-    end
   end
 
-  # These packages need their i386 version installed as well 
-  multiarchpackages = ['compat-libstdc++-33','glibc-devel','libaio','libaio-devel','libgcc','libstdc++','unixODBC','unixODBC-devel']
+  # These packages need their i386 (on RHEL5) or i686 (RHEL6+) version installed as well 
+  multiarchpackages = ['compat-libstdc++-33','glibc-devel','libaio','libaio-devel','libgcc','libstdc++','unixODBC','unixODBC-devel','openmotif']
 
-  # glibc needs to specifically have the i686 version
+  # glibc needs to specifically have the i686 version on RHEL5
   isixeightysixpackages = ['glibc']
 
   if node['kernel']['machine'] == "i686"
@@ -69,8 +51,14 @@ when "rhel"
   # Since the machine has been determined to not be 32bit
   # This should probably be changed to a 64bit check
   else
+    if node['platform_version'].to_i >= 6
+      archlist = ['x86_64','i686']
+    else
+      archlist = ['x86_64','i386']
+    end
+      
     multiarchpackages.each do |packagename|
-      ['x86_64','i386'].each do |architecture|
+      archlist.each do |architecture|
         yum_package packagename do
           arch architecture
         end
