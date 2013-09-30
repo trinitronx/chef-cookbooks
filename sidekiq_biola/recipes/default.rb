46527956bc605c066e15b74f74f8a138625dc41a
Adam Crownoble <adam.crownoble@biola.edu>
2013-09-30 09:41:30 -0700

    Updating sidekiq_biola to support individual users.
    Previously there was no way to run each process as a separate user.
    Now each app's sidekiq process can run as that app's user account.

--- sidekiq_biola/recipes/default.rb
+++ sidekiq_biola/recipes/default.rb
@@ -5,12 +5,14 @@ directory '/var/log/sidekiq' do
   action :create
 end
 
-template  '/etc/init/sidekiq.conf' do
-  source 'etc-init-sidekiq.conf.erb'
-  owner 'root'
-  group 'root'
-  mode '0644'
-  variables app_root: node[:sidekiq_biola][:app_root], environment: node[:sidekiq_biola][:environment]
+node[:sidekiq_biola][:apps].each do |app|
+  template  "/etc/init/sidekiq-#{app[:name]}.conf" do
+    source 'etc-init-sidekiq.conf.erb'
+    owner 'root'
+    group 'root'
+    mode '0644'
+    variables app_root: node[:sidekiq_biola][:app_root], environment: node[:sidekiq_biola][:environment], app: app
+  end
 end
 
 template '/etc/init/sidekiq-all.conf' do
