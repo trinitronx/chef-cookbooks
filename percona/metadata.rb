bb4ba8a97b3e16427c3f383baeb8d635c1a348d0
Jared King <jared.m.king@gmail.com>
2013-10-16 10:17:26 -0700

    percona: 1.1.2 - Disabled logging queries not using indexes in my.cnf

--- percona/metadata.rb
+++ percona/metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Biola University"
 maintainer_email "jared.king@biola.edu"
 license          "Apache 2.0"
 description      "Installs and configures Percona XtraDB Cluster"
-version          "1.1.1"
+version          "1.1.2"
 supports 				 "ubuntu", "12.04"
 %w{ apt database mysql }.each do |cb|
   depends cb
