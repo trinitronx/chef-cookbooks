0bc30714e39ce017bbb99f371214ec1e15d44fb8
troyready <troy@troyready.com>
2013-10-25 16:44:14 -0700

    selinux: update to v0.6.1; removed workaround selinux_attributes cookbook

--- selinux_attributes/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'selinux_attributes'
-maintainer       'Biola University'
-maintainer_email 'troy.ready@biola.edu'
-license          'Apache 2.0'
-description      'Installs/Configures selinux_attributes'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-depends          'selinux'
