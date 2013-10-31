91fa2d25d3bccb5cd437d3c9447ea489bf8fcfe6
Adam Crownoble <adam.crownoble@biola.edu>
2013-10-31 13:54:33 -0700

    Bump sidekiq_biola version to 0.1.1

--- sidekiq_biola/metadata.rb
+++ sidekiq_biola/metadata.rb
@@ -1,6 +1,6 @@
 maintainer       'Biola'
 maintainer_email 'adam.crownoble@biola.edu'
 description      'Installs a sidekiq Upstart script'
-version          '0.1.0'
+version          '0.1.1'
 recipe           'sidekiq_biola', 'Installs a sidekiq Upstart script'
 supports         'ubuntu'
