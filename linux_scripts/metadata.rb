3a9b49c2dc81b58712555731dc0767d77ba5041d
jasree <jim.reeves@biola.edu>
2014-02-20 14:04:37 -0800

    linux_scripts 0.5.1 minor fix to psg.erb

--- linux_scripts/metadata.rb
+++ linux_scripts/metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'troy.ready@biola.edu'
 license          'Apache 2.0'
 description      'Installs/Configures linux_scripts'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.5.0'
+version          '0.5.1'
