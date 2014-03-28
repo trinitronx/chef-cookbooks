c19a9f748dca2a8699e2eefdfc1c70b4a4bae434
Jared King <jared.king@biola.edu>
2014-03-28 11:29:27 -0700

    opsview: 1.1.2 - Added ability to specify script handler for custom scripts on Windows

--- opsview/metadata.rb
+++ opsview/metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Biola University'
 maintainer_email 'jared.king@biola.edu'
 license          'Apache 2.0'
 description      'Installs and configures Opsview Core'
-version          '1.1.1'
+version          '1.1.2'
 %w{ apt chef-vault mysql vmware yum }.each do |cb|
   depends cb
 end
