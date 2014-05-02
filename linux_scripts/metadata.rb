efe03f8dd19309eb9472386bfd9c92946a87ba63
Jared King <jared.king@biola.edu>
2014-05-02 09:15:49 -0700

    linux_scripts: 0.8.0 - Added memory hotplug script

--- linux_scripts/metadata.rb
+++ linux_scripts/metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'troy.ready@biola.edu'
 license          'Apache 2.0'
 description      'Installs/Configures linux_scripts'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.7.0'
+version          '0.8.0'
