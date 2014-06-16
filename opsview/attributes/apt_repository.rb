d5138acaacd973895ad9028980556cdfcd03cec1
jaredk <jaredk@a014206.biola.edu>
2014-06-16 13:34:51 -0700

    opsview: 1.3.0 - Allowed override of apt repository key; added IPMI checks

--- opsview/attributes/apt_repository.rb
+++ opsview/attributes/apt_repository.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: opsview
-# Recipe:: apt_repository
+# Attributes:: apt_repository
 #
 # Copyright 2014, Biola University
 #
@@ -8,7 +8,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE_2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,12 +17,5 @@
 # limitations under the License.
 #
 
-apt_repository "opsview" do
-  uri "http://downloads.opsview.com/opsview-core/latest/apt"
-  distribution node['lsb']['codename']
-  components ["main"]
-  unless node['opsview']['apt_keyserver'].empty?
-  	keyserver node['opsview']['apt_keyserver']
-  end
-  key node['opsview']['apt_key']
-end
+default['opsview']['apt_key'] = "0FC6984B"
+default['opsview']['apt_keyserver'] = "subkeys.pgp.net"
