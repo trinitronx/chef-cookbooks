b136517ce779ac7241c8891c0779c1a48fd008f6
Adam Crownoble <adam.crownoble@biola.edu>
2014-06-16 17:16:09 -0700

    Allow mulitple sidekiq queues to be configured

--- sidekiq_biola/attributes/default.rb
+++ sidekiq_biola/attributes/default.rb
@@ -2,7 +2,15 @@ default['sidekiq_biola']['app_root'] = '/srv/rack'
 default['sidekiq_biola']['apps'] = {}
 
 # Example rails app
-# default['sidekiq_biola']['apps'] << {name: 'timecard', user: 'timecard', workers: 1}
+# default['sidekiq_biola']['apps'] << {name: 'timecard', user: 'timecard'}
 
 # Example non-rails app
-# default['sidekiq_biola']['apps'] << {name: 'timecard', user: 'timecard', workers: 1, require: './config/environment.rb'}
+# default['sidekiq_biola']['apps'] << {name: 'timecard', user: 'timecard', require: './config/environment.rb'}
+
+# Example app with multiple workers
+# default['sidekiq_biola']['apps'] << {name: 'timecard', user: 'timecard', workers: 2}
+
+# Example app with multiple queues
+# Format: {name => weight}
+# Use nil for default weight
+# default['sidekiq_biola']['apps'] << {name: 'timecard', user: 'timecard', queues: {critical: 2, default: 1}}
