4b0511e3c216309969dccd2e8403163e0aa29e50
troyready <troy@troyready.com>
2014-06-19 15:42:23 -0700

    oracle: v0.11.0 - Enable optional repo via rhn cookbook on rhel 6+

--- oracle/recipes/packages.rb
+++ oracle/recipes/packages.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: oracle
 # Recipe:: packages
 #
-# Copyright 2013, Biola University 
+# Copyright 2014, Biola University 
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,37 +19,41 @@
 
 case node['platform_family']
 when "rhel"
-
+  # Some packages (e.g. sharutils) on RHEL 6 require the optional repo
+  if node['platform'] == 'redhat' and node['platform_version'].to_i > 5
+    include_recipe 'rhn::optionalchannel'
+  end
+  
   # installs supplementary required packages for Oracle
   #
   # This could be reduced to ['make','binutils','gcc','libaio','glibc-common','libstdc++','libXtst','sysstat','glibc'] for Enterprise Manager (plus the i686 version of glibc-devel), but just leaving one single list here for simplicity
-
+  
   ['binutils','elfutils-libelf','elfutils-libelf-devel','gcc','gcc-c++','glibc-common','glibc-headers','ksh','libstdc++-devel','make','sysstat','libXtst','at','sharutils'].each do |packagename|
     yum_package packagename
   end
-
+  
   # Special exceptions here for RHEL 5/6
   if node['platform_version'].to_i >= 6
     yum_package 'mksh'
   else
     yum_package 'pdksh'
   end
-
+  
   # These packages need their i386 (on RHEL5) or i686 (RHEL6+) version installed as well 
   multiarchpackages = ['compat-libstdc++-33','glibc-devel','libaio','libaio-devel','libgcc','libstdc++','unixODBC','unixODBC-devel','openmotif','openmotif22']
-
+  
   # glibc needs to specifically have the i686 version on RHEL5
   isixeightysixpackages = ['glibc']
-
+  
   if node['kernel']['machine'] == "i686"
     multiarchpackages.each do |packagename|
       yum_package packagename
     end
-  
+    
     isixeightysixpackages.each do |packagename|
       yum_package packagename
     end
-
+  
   # Since the machine has been determined to not be 32bit
   # This should probably be changed to a 64bit check
   else
@@ -66,7 +70,7 @@ when "rhel"
         end
       end
     end
-
+    
     isixeightysixpackages.each do |packagename|
       ['x86_64','i686'].each do |architecture|
         yum_package packagename do
